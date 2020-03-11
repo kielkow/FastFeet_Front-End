c5fa816579e7adffe22c09f0e3b6fa41a9b61a8f
kielkow <matheuskiel@fiorifer.com.br>
2020-03-11 13:11:53 -0300

    Using Unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,25 +1,31 @@
+/* eslint-disable no-console */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.png';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="FastFeet" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <span>E-MAIL</span>
-        <input type="email" placeholder="Your e-mail..." />
+        <Input name="email" type="email" placeholder="Your e-mail..." />
 
         <span>PASSWORD</span>
-        <input type="password" placeholder="Your password..." />
+        <Input name="password" type="password" placeholder="Your password..." />
 
         <button type="submit">Enter</button>
         <Link to="/register">Create free account</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,28 +1,34 @@
+/* eslint-disable no-console */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/logo.png';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <>
       <img src={logo} alt="FastFeet" />
 
-      <form>
+      <Form onSubmit={handleSubmit}>
         <span>NAME</span>
-        <input placeholder="Your full name..." />
+        <Input name="name" placeholder="Your full name..." />
 
         <span>E-MAIL</span>
-        <input type="email" placeholder="Your e-mail..." />
+        <Input name="email" type="email" placeholder="Your e-mail..." />
 
         <span>PASSWORD</span>
-        <input type="password" placeholder="Your password..." />
+        <Input name="password" type="password" placeholder="Your password..." />
 
         <button type="submit">Create account</button>
         <Link to="/">I already have an account</Link>
-      </form>
+      </Form>
     </>
   );
 }
