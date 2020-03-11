c9b2ea4c17a5aeb175b3f374c3a0bb23d12a7e8e
kielkow <matheuskiel@fiorifer.com.br>
2020-03-11 12:58:29 -0300

    Authentication style

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.png';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <img src={logo} alt="FastFeet" />
+
+      <form>
+        <span>E-MAIL</span>
+        <input type="email" placeholder="Your e-mail..." />
+
+        <span>PASSWORD</span>
+        <input type="password" placeholder="Your password..." />
+
+        <button type="submit">Enter</button>
+        <Link to="/register">Create free account</Link>
+      </form>
+    </>
+  );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo.png';
 
 // import { Container } from './styles';
 
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <img src={logo} alt="FastFeet" />
+
+      <form>
+        <span>NAME</span>
+        <input placeholder="Your full name..." />
+
+        <span>E-MAIL</span>
+        <input type="email" placeholder="Your e-mail..." />
+
+        <span>PASSWORD</span>
+        <input type="password" placeholder="Your password..." />
+
+        <button type="submit">Create account</button>
+        <Link to="/">I already have an account</Link>
+      </form>
+    </>
+  );
 }
