01e30414f5a45833f1d463c1cada2726de19781a
kielkow <matheuskiel@fiorifer.com.br>
2020-03-19 12:31:38 -0300

    Register on application

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -32,4 +32,27 @@ export function* signIn({ payload }) {
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    toast.success('User created!');
+    history.push('/');
+  } catch (err) {
+    toast.error('Registration failure, please verify your data');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
