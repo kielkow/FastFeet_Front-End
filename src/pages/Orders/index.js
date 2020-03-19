5593ee4501f8585bbd83454bdb0dd3f0bf5b2c7c
kielkow <matheuskiel@fiorifer.com.br>
2020-03-19 12:40:23 -0300

    Set token in all requisitions

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 // import { Container } from './styles';
 
+import api from '~/services/api';
+
 export default function Orders() {
+  api.get('orders');
   return <div>Orders</div>;
 }
