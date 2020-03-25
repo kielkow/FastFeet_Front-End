46f984c61f5434f823ebd4d748b05f9518edaf26
kielkow <matheuskiel@fiorifer.com.br>
2020-03-24 21:43:10 -0300

    Style Problems page

--- src/pages/Problems/index.js
+++ src/pages/Problems/index.js
@@ -1,7 +1,311 @@
-import React from 'react';
+/* eslint-disable array-callback-return */
+/* eslint-disable prefer-const */
+/* eslint-disable no-alert */
+/* eslint-disable no-restricted-globals */
+import React, { useState, useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+import { Input } from '@rocketseat/unform';
+import { MdAdd, MdEdit, MdDelete } from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { makeStyles } from '@material-ui/core/styles';
+// import Options from '../../components/Options';
 
-// import { Container } from './styles';
+// import { toast } from 'react-toastify';
+// import history from '~/services/history';
+import { Container, Content, Pagination, Previous, Next } from './styles';
+
+import api from '~/services/api';
+
+// import * as StudentActions from '../../store/modules/student/actions';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    fontSize: '20px',
+    fontWeight: 'bold',
+    color: '#989898',
+    padding: '0',
+    width: '20px',
+  },
+}));
 
 export default function Problems() {
-  return <div>Problems</div>;
+  const [problems, setProblems] = useState([]);
+  let [page, setPage] = useState(1);
+  const [loadingNext, setLoadingNext] = useState(false);
+  const [finalPage, setFinalPage] = useState(false);
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  // const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function loadProblems() {
+      const response = await api.get('/ordersproblems', {
+        params: {
+          page,
+        },
+      });
+
+      const { data } = response;
+
+      setProblems(data);
+
+      const checkFinalPage = await api.get('/ordersproblems', {
+        params: {
+          page: page + 1,
+        },
+      });
+
+      if (checkFinalPage.data.length === 0) {
+        setLoadingNext(false);
+        setFinalPage(true);
+      } else {
+        setLoadingNext(false);
+        setFinalPage(false);
+      }
+    }
+
+    loadProblems();
+  }, [page]);
+
+  /*
+  async function reloadProblems() {
+    const response = await api.get('/ordersproblems', {
+      params: {
+        page,
+      },
+    });
+
+    const { data } = response;
+
+    setProblems(data);
+
+    const checkFinalPage = await api.get('/ordersproblems', {
+      params: {
+        page: page + 1,
+      },
+    });
+
+    if (checkFinalPage.data.length === 0) {
+      setLoadingNext(false);
+      setFinalPage(true);
+    } else {
+      setLoadingNext(false);
+      setFinalPage(false);
+    }
+  }
+
+  async function deleteproblem(e) {
+    const confirm = window.confirm('Do you really wish delete this problem?');
+
+    if (confirm) {
+      try {
+        await api.delete(`/ordersproblems/${e}`);
+        toast.info(
+          'Not possible delete a problem, please check the info about it'
+        );
+        history.push('/ordersproblems');
+        reloadProblems();
+      } catch (err) {
+        toast.error(
+          'Not possible delete a problem, please check the info about it'
+        );
+      }
+    }
+  }
+  */
+
+  /*
+  function editRequest(student) {
+    dispatch(StudentActions.updateStudentRequest(student));
+  }
+  */
+
+  async function next() {
+    setLoadingNext(true);
+
+    setPage((page += 1));
+
+    const pageProblems = await api.get('/ordersproblems', {
+      params: {
+        page,
+      },
+    });
+
+    const checkFinalPage = await api.get('/ordersproblems', {
+      params: {
+        page: page + 1,
+      },
+    });
+
+    if (checkFinalPage.data.length === 0) {
+      setProblems(pageProblems.data);
+      setLoadingNext(false);
+      setFinalPage(true);
+    } else {
+      setLoadingNext(false);
+      setFinalPage(false);
+    }
+  }
+
+  async function previous() {
+    setLoadingNext(true);
+    setFinalPage(false);
+
+    if (page !== 1) {
+      setPage((page -= 1));
+    }
+
+    const pageProblems = await api.get('/ordersproblems', {
+      params: {
+        page,
+      },
+    });
+
+    setProblems(pageProblems.data);
+    setLoadingNext(false);
+  }
+
+  async function searchproblem(e) {
+    if (e.target.value === '' || e.target.value === null) {
+      const originalProblems = await api.get('/ordersproblems');
+      setProblems(originalProblems.data);
+      return;
+    }
+    const similarProblems = await api.get('/ordersproblems', {
+      params: {
+        name: e.target.value,
+      },
+    });
+    setProblems(similarProblems.data);
+  }
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Container>
+      <strong>Management Problems</strong>
+      <header>
+        <Input
+          name="name"
+          type="text"
+          placeholder="&#xf002; Search for problems..."
+          style={{ fontFamily: 'Arial, FontAwesome' }}
+          onChange={searchproblem}
+        />
+        <div>
+          <Link to="/registerproblem">
+            <MdAdd color="#fff" size={18} />
+            <span>Register</span>
+          </Link>
+        </div>
+      </header>
+      <Content>
+        <header>
+          <span>Order</span>
+          <span>Problem</span>
+          <span>Actions</span>
+          <span />
+        </header>
+        <ul>
+          {problems.length === 0 ? (
+            <span style={{ color: '#444444' }}> Any problem found...</span>
+          ) : (
+            problems.map(problem => (
+              <li key={problem.id}>
+                <span>#{problem.order_id}</span>
+                <span>{problem.description}</span>
+                <div style={{ marginRight: '50px', boxShadow: 'none' }}>
+                  {/*
+                <Link
+                  id="edit"
+                  to="/editproblem"
+                  // onClick={() => editRequest(problem)}
+                >
+                  edit
+                </Link>
+                <button
+                  id="delete"
+                  type="button"
+                  onClick={() => deleteproblem(problem.id)}
+                  value={problem.id}
+                >
+                  delete
+                </button> */}
+                  <Button
+                    aria-controls="simple-menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                    className={classes.root}
+                  >
+                    ...
+                  </Button>
+                  <Menu
+                    id="simple-menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                  >
+                    <MenuItem onClick={handleClose}>
+                      <MdEdit
+                        color="#7d40e7"
+                        size={18}
+                        style={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          marginRight: '10px',
+                        }}
+                      />
+                      <span>Edit</span>
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <MdDelete
+                        color="#de3b3b"
+                        size={18}
+                        style={{
+                          display: 'flex',
+                          alignItems: 'center',
+                          marginRight: '10px',
+                        }}
+                      />
+                      <span>Delete</span>
+                    </MenuItem>
+                  </Menu>
+                </div>
+              </li>
+            ))
+          )}
+        </ul>
+      </Content>
+      <Pagination>
+        <Previous
+          type="button"
+          onClick={() => previous()}
+          page={page}
+          loadingNext={loadingNext}
+        >
+          Previous
+        </Previous>
+        <span>{page}</span>
+        <Next
+          type="button"
+          onClick={() => next()}
+          loadingNext={loadingNext}
+          finalPage={finalPage}
+        >
+          Next
+        </Next>
+      </Pagination>
+    </Container>
+  );
 }
---
+++ src/pages/Problems/styles.js
@@ -0,0 +1,183 @@
+import styled from 'styled-components';
+import { lighten, darken } from 'polished';
+
+export const Container = styled.div`
+  max-width: 1000px;
+  margin: 30px auto;
+  display: flex;
+  flex-direction: column;
+  strong {
+    font-size: 24px;
+    color: ${lighten(0.03, '#444444')};
+    margin-bottom: 40px;
+  }
+  header {
+    display: flex;
+    justify-content: space-between;
+    input {
+      width: 240px;
+      padding: 8px 10px;
+      border-radius: 1px;
+      border: 0.5px solid ${lighten(0.6, '#444444')};
+    }
+    div {
+      display: flex;
+      justify-content: center;
+      a {
+        background: #7d40e7;
+        color: #fff;
+        padding: 5px 10px;
+        border: 0;
+        font-weight: bold;
+        border-radius: 4px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        &:hover {
+          background: ${darken(0.05, '#7d40e7')};
+        }
+      }
+    }
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  margin-top: 20px;
+  border-radius: 4px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  header {
+    display: flex;
+    justify-content: left;
+    max-width: 1000px;
+    font-weight: bold;
+    margin-bottom: 20px;
+    padding-left: 15px;
+  }
+  ul {
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    li {
+      display: flex;
+      align-items: center;
+      padding: 15px;
+      border-radius: 4px;
+      border-bottom: 1px solid #eee;
+      background: #fff;
+      & + li {
+        margin-top: 15px;
+      }
+      color: ${lighten(0.03, '#444444')};
+      div {
+        display: flex;
+        #edit {
+          color: #4d85ee;
+        }
+        #delete {
+          color: #de3b3b;
+        }
+        button {
+          border: 0;
+          background: none;
+          & + button {
+            margin-left: 20px;
+          }
+        }
+        a {
+          border: 0;
+          background: none;
+          & + button {
+            margin-left: 20px;
+          }
+        }
+      }
+    }
+    li > span:first-child {
+      width: 20.5%;
+    }
+    li > span:first-child + span {
+      width: 75%;
+    }
+    li > span:first-child + span + span {
+      width: 50%;
+    }
+    li > div {
+      width: 0%;
+    }
+  }
+  header > span:first-child {
+    width: 22%;
+  }
+  header > span:first-child + span {
+    width: 80%;
+  }
+  header > span:first-child + span + span {
+    width: 7%;
+  }
+`;
+
+export const Pagination = styled.div`
+  margin-top: 15px;
+  display: flex;
+  align-items: center;
+  span {
+    color: #7d40e7;
+    font-weight: bold;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+`;
+
+export const Previous = styled.button.attrs(props => ({
+  type: 'button',
+  disabled: props.page === 1 || props.loadingNext,
+}))`
+  background: #7d40e7;
+  color: #eee;
+  font-style: bold;
+  & + button {
+    margin-left: 10px;
+  }
+  font-size: 17px;
+  float: center;
+  display: flex;
+  padding: 10px 15px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+  &:hover {
+    background: ${darken(0.05, '#7d40e7')};
+  }
+`;
+
+export const Next = styled.button.attrs(props => ({
+  type: 'button',
+  disabled: props.loadingNext || props.finalPage,
+}))`
+  background: #7d40e7;
+  color: #eee;
+  font-style: bold;
+  & + button {
+    margin-left: 10px;
+  }
+  font-size: 17px;
+  float: center;
+  display: flex;
+  padding: 10px 15px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  &[disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+  &:hover {
+    background: ${darken(0.05, '#7d40e7')};
+  }
+`;
