f43c1b0ce8e0e15d186c50baf2cb9952f9df12f5
kielkow <matheuskiel@fiorifer.com.br>
2020-04-07 12:21:26 -0300

    Style AvatarInput on Courier edit page

--- src/pages/Couriers/Edit/index.js
+++ src/pages/Couriers/Edit/index.js
@@ -1,10 +1,103 @@
-import React from 'react';
+/* eslint-disable consistent-return */
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Input } from '@rocketseat/unform';
+import { MdArrowBack, MdSave } from 'react-icons/md';
+
+import { toast } from 'react-toastify';
+import { Link } from 'react-router-dom';
+import api from '~/services/api';
+import history from '~/services/history';
+
+import AvatarInput from './AvatarInput';
+
+import { Container, Content } from './styles';
 
 export default function EditCourier() {
-  const courierEdit = useSelector(state => state.courier.courier);
+  const courier = useSelector(state => state.courier.courier);
 
-  console.log('COURIER', courierEdit);
+  const [courierEdit, setCourierEdit] = useState(courier);
 
-  return <div>Edit Courier</div>;
+  function handleChangeName(e) {
+    setCourierEdit({
+      id: courierEdit.id,
+      name: e.target.value,
+      avatar_id: courierEdit.avatar_id,
+      email: courierEdit.email,
+    });
+  }
+
+  function handleChangeEmail(e) {
+    setCourierEdit({
+      id: courierEdit.id,
+      name: courierEdit.name,
+      avatar_id: courierEdit.avatar_id,
+      email: e.target.value,
+    });
+  }
+
+  async function updateCourier() {
+    const updatedCourier = {
+      name: courierEdit.name,
+      avatar_id: courierEdit.avatar_id,
+      email: courierEdit.email,
+    };
+    const arrayCourier = Object.values(updatedCourier);
+    let isNull = false;
+    arrayCourier.forEach(propCourier => {
+      if (propCourier === null || propCourier === '' || propCourier === 0)
+        isNull = true;
+    });
+
+    if (isNull) return toast.error('Please check courier´s information');
+
+    try {
+      await api.put(`/couriers/${courierEdit.id}`, updatedCourier);
+      toast.success('Courier updated with success!');
+      history.push('/couriers');
+    } catch (err) {
+      toast.error('Not possible update this courier');
+    }
+  }
+
+  return (
+    <Container>
+      <header>
+        <strong>Edit Courier</strong>
+        <div>
+          <Link type="button" to="/couriers">
+            <MdArrowBack color="#fff" size={18} />
+            <span>Back</span>
+          </Link>
+          <button type="button" onClick={updateCourier}>
+            <MdSave color="#fff" size={18} />
+            <span>Save</span>
+          </button>
+        </div>
+      </header>
+      <Content>
+        <AvatarInput name="avatar_id" />
+
+        <div>
+          <span>Name</span>
+          <Input
+            name="name"
+            placeholder="Courier name..."
+            value={courierEdit.name}
+            onChange={handleChangeName}
+          />
+        </div>
+        <div>
+          <span>E-mail</span>
+          <Input
+            name="email"
+            type="email"
+            placeholder="Courier e-mail..."
+            value={courierEdit.email}
+            onChange={handleChangeEmail}
+          />
+        </div>
+      </Content>
+    </Container>
+  );
 }
